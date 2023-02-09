e32b473f35c0a6045844397f39b68d80a6a283c8
Egor Kryuchkov <xopek17@mail.ru>
2023-02-09 14:29:59 +0300

    if construction replaced by assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,12 +1,7 @@
 from capitalize import capitalize
 
-if capitalize('hello') == 'Hello':
-    raise Exception('БЛЯЯЯЯТЬ')
+assert capitalize('hello') == 'Hello'
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
